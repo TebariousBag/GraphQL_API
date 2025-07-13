bdc36d5415e625c167f6cbe66aab53909cd68847
“Tristian <tristian.davis@atlasschool.com>
2025-07-13 13:57:02 -0500

    try this

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -10,6 +10,6 @@ app.use('/graphql',graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
-app.listen(5500,()=>{
+app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
 });
